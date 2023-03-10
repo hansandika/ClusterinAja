51be1de2939c29593413e0a0e1b5ac9bc638ec81
hansandika <hansandika70@gmail.com>
2023-03-10 16:12:46 +0700

    change services redirect

--- config/services.php
+++ config/services.php
@@ -34,6 +34,6 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'https://clusterinaja.herokuapp.com/login/google/callback',
+        'redirect' => 'https://clusterinaja.up.railway.app/login/google/callback',
     ],
 ];
